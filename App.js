e16bca7d3b95f001482634a4f751c9f37fab903c
Sagar Musale <sagar1991.musale@gmail.com>
2017-12-07 23:48:46 +0530

    Changes for Firebase init

--- App.js
+++ App.js
@@ -11,6 +11,18 @@ import {
   Text,
   View
 } from 'react-native';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyDhrAm8bI3Eg2g1IzYMDFQAmZfb12RzxtQ",
+  authDomain: "app-moneywire.firebaseapp.com",
+  databaseURL: "https://app-moneywire.firebaseio.com",
+  projectId: "app-moneywire",
+  storageBucket: "",
+  messagingSenderId: "84141118544"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
