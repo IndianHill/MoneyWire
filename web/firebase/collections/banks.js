94d42f08f723e42c921d8b1d5afa28bd2daa6d28
Sagar Musale <sagar1991.musale@gmail.com>
2018-01-05 01:40:14 +0530

    added jquery for serialization and other uses

--- web/firebase/collections/banks.js
+++ web/firebase/collections/banks.js
@@ -18,3 +18,29 @@ function getAllBanks() {
         successfullyFetchedBanks(allBanks)
     });
 }
+
+/*
+ *  Creating bank 
+ */
+function createBank(bankData) {
+    var user = firebase.auth().currentUser;
+    firestore = firebase.firestore();
+    firestore.collection(cl_banks).add({
+        bankName: bankData.name,
+        bankWebsite: bankData.website,
+        bankCustomerCare: bankData.customerCare,
+        bankTollFree: bankData.tollFree,
+        bankFacebook: bankData.fb,
+        bankGooglePlus: bankData.googlePlus,
+        bankTwitter: bankData.twitter,
+        bankLinkedIn: bankData.linkedIn,
+        bankCreatedBy: user.uid,
+        bankUpdatedBy: user.uid
+    })
+    .then(function() {
+        console.log("Bank successfully created!");
+    })
+    .catch(function(error) {
+        console.error("Error creating bank: ", error);
+    });
+}
