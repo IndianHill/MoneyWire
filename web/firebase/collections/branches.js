09507b6a172f1888758a3c4e8772a5789f93e1ef
Sagar Musale <sagar1991.musale@gmail.com>
2018-01-19 01:55:02 +0530

    Creating branch object in firestore within bank

--- web/firebase/collections/branches.js
+++ web/firebase/collections/branches.js
@@ -19,3 +19,35 @@ function getBranchesOfBank(bankKey) {
         //successfullyFetchedBanks(allBranches)
     });
 }
+
+/**
+ *  Create bank's branch
+ */
+function createBankBranch(branchData, bankKey) {
+    var user = firebase.auth().currentUser;
+    firestore = firebase.firestore();
+    firestore.collection(cl_banks).doc(bankKey).collection(cl_branches).add({
+        "name": branchData.name,
+        "street": branchData.street,
+        "city": branchData.city,
+        "district": branchData.district,
+        "state": branchData.state,
+        "pinCode": branchData.pinCode,
+        "fullAddress": branchData.fullAddress,
+        "contactNumber": branchData.contactNumber,
+        "ifsc": branchData.ifscCode,
+        "micr": branchData.micrCode,
+        "createdBy": user.uid,
+        "updatedBy": user.uid,
+        "createdAt": firebase.firestore.FieldValue.serverTimestamp(),
+        "updatedAt": firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then(function(branch) {
+        console.log("Branch successfully created with id:"+branch.id);
+        branchCreatedSuccessfully(branch);
+    })
+    .catch(function(error) {
+        console.log("Error creating branch: "+error);
+        errorWhileCreatingBranch(error);
+    });
+}
