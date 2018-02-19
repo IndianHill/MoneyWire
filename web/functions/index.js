b48c7441bb130c632366ef2eb0f497332fe4aa66
Sagar Musale <sagar1991.musale@gmail.com>
2018-02-20 02:41:13 +0530

    Added cloud functions to get lat-lng

---
+++ web/functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDhrAm8bI3Eg2g1IzYMDFQAmZfb12RzxtQ&address='
+
+exports.updateBranchLatLng = functions.firestore.document('/branches/{branch}').onWrite((event)=>{
+
+    fullAddress = event.data.data().fullAddress;
+    console.log('Requesting lat lng for address:'+fullAddress);
+    const get_url = encodeURI(BASE_URL+fullAddress);
+    
+    var request = require("request");
+    request(get_url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            jsonBody = JSON.parse(body)
+            const lat = jsonBody['results'][0]['geometry']['location']['lat'];
+            const lng = jsonBody['results'][0]['geometry']['location']['lng'];
+            console.log('Lat:'+lat+' Lng:'+lng+' FullAddress:'+fullAddress);
+
+            event.data.ref.set({
+                lat: lat,
+                lng: lng
+                }, {merge: true});
+        } else {
+            console.log('Error:'+JSON.stringify(error))
+        }
+    });
+    return true;
+});
