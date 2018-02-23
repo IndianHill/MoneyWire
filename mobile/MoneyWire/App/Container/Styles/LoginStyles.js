8d2800b10354406a080d0c5c701b07b0e89d1c4a
Sagar Musale <sagar1991.musale@gmail.com>
2018-02-24 04:08:54 +0530

    Added react-native basic

---
+++ mobile/MoneyWire/App/Container/Styles/LoginStyles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native'
+import { Colors, Fonts } from '../../Theme/'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems:'center',
+        backgroundColor: Colors.primary,
+    },
+    logoTitle: {
+        top: '35%',
+        fontSize: 32,
+        color: Colors.white,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        // fontFamily: 'AvertaRegular',
+    },
+    bottomView: {
+        flex: 1,
+        position: 'absolute',
+        bottom: '10%',
+        flexDirection: 'row',
+        marginLeft: '8%',
+        marginRight: '8%',
+        justifyContent: 'space-between',
+    },
+    loginIconImgView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loginIconImg: {
+        height: 32,
+        width: 32,
+        tintColor: Colors.white,
+    },
+    mobileLoginIconImg: {
+        height: 32,
+        width: 20,
+        tintColor: Colors.white,
+    },
+    loadingView: {
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+})
