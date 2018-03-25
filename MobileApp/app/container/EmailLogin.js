cad06015237bebede342e06463e0aef2462916bf
Sagar Musale <sagar1991.musale@gmail.com>
2018-03-25 17:59:04 +0530

    Showing email login view

--- MobileApp/app/container/EmailLogin.js
+++ MobileApp/app/container/EmailLogin.js
@@ -16,6 +16,9 @@ class EmailLogin extends Component {
         this.state = {
             isLoading: false,
             currentUser: {},
+            isNewUser: false,
+            email: '',
+            password: '',
         };
     }
 
@@ -26,6 +29,69 @@ class EmailLogin extends Component {
                 currentUser: user
             })
         }
+    }
+
+    setEmailId = (text) => {
+        this.setState({
+            email: text
+        })
+    }
+
+    setPwd = (text) => {
+        this.setState({
+            password: text
+        })
+    }
+
+    emailLogin = () => {
+
+        let email = this.state.email
+        let pwd = this.state.password
+
+        console.log('Email:'+email+" pwd:"+pwd)
+
+        // TODO: Verify Email-id and password strength
+
+
+    }
+
+    getSignTitleText = () => {
+        if(this.state.isNewUser) {
+            return "Register with your email address"
+        } else {
+            return "Login with your email address"
+        }
+    }
+
+    getSignInText = () => {
+        if(this.state.isNewUser) {
+            return "Register"
+        } else {
+            return "Sign In"
+        }
+    }
+
+    getToggleMsg = () => {
+        if(this.state.isNewUser) {
+            return "Already registered? "
+        } else {
+            return "Don't have an account? "
+        }
+    }
+
+    getToggleBtnText = () => {
+        if(this.state.isNewUser) {
+            return "Login"
+        } else {
+            return "Register"
+        }
+    }
+
+    toggleNewUser = () => {
+
+        this.setState({
+            isNewUser: !this.state.isNewUser
+        })
     }
 
     //--------------------------- Render UI ---------------------------
@@ -43,10 +109,80 @@ class EmailLogin extends Component {
         )
     }
 
+    getRetypePwdTextView = () => {
+        if (this.state.isNewUser) {
+            return (
+                <TextField
+                    label='Retype Password'
+                    textColor={Colors.grey_dark}
+                    labelTextStyle={styles.signInTextViewStyle}
+                    value={this.state.phone}
+                    keyboardType='email-address'
+                    secureTextEntry
+                    maxLength={10}
+                    onChangeText={ (text) => this.setPwd(text) }
+                />
+            )
+        }
+    }
+
+    renderLoginMsg = () => {
+        return (
+            <View style={styles.loginMsgViewStyle}>
+                <Text style={styles.loginMsgText}>{ this.getSignTitleText() }</Text>
+            </View>
+        )
+    }
+
+    renderSignInView = () => {
+        return (
+            <View style={styles.signInViewStyle}>
+                <TextField
+                    label='Email'
+                    textColor={Colors.grey_dark}
+                    labelTextStyle={styles.signInTextViewStyle}
+                    value={this.state.phone}
+                    keyboardType='email-address'
+                    maxLength={10}
+                    onChangeText={ (text) => this.setEmailId(text) }
+                />
+                <TextField
+                    label='Password'
+                    textColor={Colors.grey_dark}
+                    labelTextStyle={styles.signInTextViewStyle}
+                    value={this.state.phone}
+                    keyboardType='email-address'
+                    secureTextEntry
+                    maxLength={10}
+                    onChangeText={ (text) => this.setPwd(text) }
+                />
+                { this.getRetypePwdTextView() }
+                <TouchableOpacity onPress={ () => { this.emailLogin() }}
+                        style={styles.signInBtnStyle}>
+                    <Text style={styles.signInBtnTextStyle}>{ this.getSignInText() }</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    renderBottomView = () => {
+        return (
+            <View style={styles.bottomViewStyle}>
+                <Text style={styles.bottomMsgTextStyle}>{ this.getToggleMsg() }</Text>
+                <TouchableOpacity onPress={ () => { this.toggleNewUser() }}>
+                    <Text textDecorationLine='underline' style={styles.toggleTextStyle}>{ this.getToggleBtnText() }</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     render () {
         return (
             <View style={styles.container}>
                 { this.renderNavBar() }
+                { this.renderLoginMsg() }
+                { this.renderSignInView() }
+                { this.renderBottomView() }
             </View>
         )
     }
--- MobileApp/app/container/Styles/EmailLoginStyles.js
+++ MobileApp/app/container/Styles/EmailLoginStyles.js
@@ -28,4 +28,64 @@ export default StyleSheet.create({
     backBtn: {
         position:'absolute', top: 28, left: 10
     },
+    loginMsgViewStyle:{
+        top: '10%',
+        marginRight: '8%',
+        marginLeft: '8%',
+        justifyContent: 'center', 
+    },
+    loginMsgText:{
+        fontSize: 32,
+        color: Colors.primary,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        fontFamily: Fonts.regular,
+    },
+    signInViewStyle: {
+        top: '15%',
+        marginRight: '8%',
+        marginLeft: '8%',
+        justifyContent: 'center', 
+    },
+    signInTextViewStyle: {
+        fontSize: 16,
+        color: Colors.grey_dark,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        fontFamily: Fonts.light,
+    },
+    signInBtnStyle:{
+        width: '100%',
+        marginTop: '5%',
+        marginRight: '8%',
+        marginLeft: '8%',
+        height: 35,  
+        borderRadius: 10,
+        backgroundColor: Colors.primary,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
+    signInBtnTextStyle: {
+        color: Colors.white,
+        fontSize: 16,
+        fontFamily: Fonts.regular,
+    },
+    bottomViewStyle: {
+        bottom: '8%',
+        marginRight: '8%',
+        marginLeft: '8%',
+        flexDirection: 'row',
+        position:'absolute',
+    },
+    bottomMsgTextStyle:{
+        fontSize: 16,
+        color: Colors.black,
+        fontFamily: Fonts.light,
+    },
+    toggleTextStyle: {
+        fontSize: 16,
+        color: Colors.primary,
+        fontFamily: Fonts.light,
+    }
 })
